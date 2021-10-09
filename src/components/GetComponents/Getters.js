97165099820d265399ad790d05494df928c186c3
jhoacar <jhoancarrero@gmail.com>
2021-10-08 23:37:25 -0300

    Se ajusta con el servidor

--- src/components/GetComponents/Getters.js
+++ src/components/GetComponents/Getters.js
@@ -3,7 +3,7 @@ import { getImage } from './StorageIndexedDB';
 const routeImages = "/images/";
 
 export const fetchData = function (setData) {
-  fetch('/data.json', { headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' } })
+  fetch('/data', { headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' } })
     .then(function (response) {
       console.log(response);
       return response.json();
